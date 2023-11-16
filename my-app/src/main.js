046e080b540b1ab05c9055b3838fa6dbd93e81c3
kkxxll <fflinkexin@chinatelecom.cn>
2023-11-16 16:37:43 +0800

    feat:注册全局组件

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+import HelloWorld from './components/HelloWorld.vue'
+
+const _HelloWorld  = app.component('HelloWorld', HelloWorld)
+app.use(_HelloWorld)
+
+app.mount('#app')
