0b351582504f8cbefcea87faed54315bb4fea1ed
kkxxll <fflinkexin@chinatelecom.cn>
2023-11-17 16:20:59 +0800

    feat:Icon组件封装

--- my-app/packages/components/Icon.vue
+++ my-app/packages/components/Icon.vue
@@ -1,8 +1,35 @@
 <template>
-    <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-fabu"></use>
+    <svg :class="svgClass" aria-hidden="true" @click="handleClick">
+        <use :xlink:href="iconName"></use>
     </svg>
 </template>
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+    props: {
+        svgClass: {
+            type: String,
+            default: ''
+        },
+        iconName: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        const svgClass = ref(`icon ${props.svgClass}`)
+        const iconName = ref(`#${props.iconName}`)
+        const handleClick = () => {
+            context.emit('onClick')
+        }
+        return {
+            svgClass,
+            iconName,
+            handleClick
+        }
+    }
+})
+</script>
 <style scoped>
 .icon {
     width: 1em;
@@ -10,5 +37,6 @@
     vertical-align: -0.15em;
     fill: currentColor;
     overflow: hidden;
+    cursor: pointer;
 }
 </style>
--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,8 +2,23 @@
 </script>
 
 <template>
-  <div><Icon /></div>
+  <div>
+    <Icon iconName="icon-fabu" @onClick="handleClick" />
+  </div>
 </template>
+<script>
+import { defineComponent, ref } from 'vue'
 
+export default defineComponent({
+    setup(props, context) {
+        const handleClick = () => {
+          console.log('click event')
+        }
+        return {
+          handleClick
+        }
+    }
+})
+</script>
 <style scoped>
 </style>
