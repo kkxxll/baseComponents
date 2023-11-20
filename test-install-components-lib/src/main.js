8d564ba5f52ce77d081fb3033761196538018aea
kkxxll <fflinkexin@chinatelecom.cn>
2023-11-20 15:26:56 +0800

    feat:包安装和使用

--- test-install-components-lib/src/main.js
+++ test-install-components-lib/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import myTinyComponents from 'my-app-tiny-components'
+import 'my-app-tiny-components/dist/style.css'
 
-createApp(App).mount('#app')
+createApp(App).use(myTinyComponents).mount('#app')
